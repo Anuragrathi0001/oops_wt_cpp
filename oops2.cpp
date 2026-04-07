598e8225067622335ad3a4d5f8210043a39aa128
Anuragrathi0001 <rathisanjita32@gmail.com>
2026-04-07 11:40:07 +0530

    oops2

---
+++ oops2.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student{
+    string name;
+    int roll;
+    public:
+    Student(string n,int r){
+        name = n;
+        roll = r;
+    }
+};
+int main(){
+    Student s1("Anurag", 123);
+}
